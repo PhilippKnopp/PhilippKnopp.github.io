a2525db0853bafbec035b1cfb493a7241f44d3c2
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2019-07-26 14:03:49 +0200

    Add files via upload

--- classes.js
+++ classes.js
@@ -2,5 +2,23 @@ class Figure extends Phaser.GameObjects.Sprite {
     constructor (config, key) {
         super(config.scene, config.x, config.y, key);
         config.scene.add.existing(this);
+        
+        this.onTile;
+        this.active = false;
+        
+        this.setInteractive();
+        this.on("pointerup", this.activateFigure, this);
+        
+        this.
     }
+    
+    activateFigure() {
+        for (var i = 0; i < figuresOnMap.length, i++) {
+            figuresOnMap[i].active = false;
+            figuresOnMap[i].setFrame(0);
+        }
+        this.active = true;
+        this.setFrame(1);
+    }
+    
 }
