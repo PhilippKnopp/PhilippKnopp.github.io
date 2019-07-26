1b17850f26b01f888518a020280d827966183e66
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2019-07-26 13:23:50 +0200

    Add files via upload
    New .js File!

---
+++ classes.js
@@ -0,0 +1,6 @@
+class Figure extends Phaser.GameObjects.Sprite {
+    constructor (config, key) {
+        super(config.scene, config.x, config.y, key);
+        config.scene.add.existing(this);
+    }
+}
