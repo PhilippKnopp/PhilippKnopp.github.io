1591a663f39518a70244dbae8189dc4f924cfe48
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2019-08-09 16:00:50 +0200

    Add files via upload

--- classes.js
+++ classes.js
@@ -29,6 +29,7 @@ class Figure extends Phaser.GameObjects.Sprite {
         this.dieSize;
         
         this.moveNow = function () {
+            console.log(this.pathToTravel);
             if (this.pathToTravel.length > 0 && this == barb) {
                 movementTweenBarb.data[0].start = this.x;
                 movementTweenBarb.data[1].start = this.y;
