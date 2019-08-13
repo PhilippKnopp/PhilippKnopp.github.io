f6fa660f51b2d04266bbd3b9d9fc6fbf6a4f6465
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2019-08-13 12:00:48 +0200

    Add files via upload

--- classes.js
+++ classes.js
@@ -19,15 +19,9 @@ class Figure extends Phaser.GameObjects.Sprite {
         };
         
         this.enterTile = function () {
-            console.log(this.pathToTravel);
-            console.log(this.onTile);
             tileArray[this.onTile].occupiedBy = "";
             this.onTile = this.pathToTravel[0];
-            console.log(this.pathToTravel);
-            console.log(this.onTile);
             this.pathToTravel.shift();
-            console.log(this.pathToTravel);
-            console.log(this.onTile);
             tileArray[this.onTile].occupiedBy = "figure";
         };
         
