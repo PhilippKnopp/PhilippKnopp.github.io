fd5c61325a8ec34463b81904bc36231079df2306
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2019-09-23 08:16:35 +0200

    Add files via upload

--- heroines.js
+++ heroines.js
@@ -20,6 +20,7 @@ class Figure extends Phaser.GameObjects.Sprite {
         this.on("pointerover", this.showFace, this);
         
         this.setOnMap = function () {
+            console.log("Figur setOnMap()");  // <––––––––––––––––––––––––––––––––––
             tileArray[this.onTile].occupiedBy = "figure";
             this.setAlpha(1);
             this.x = tileArray[this.onTile].x;
@@ -27,6 +28,7 @@ class Figure extends Phaser.GameObjects.Sprite {
         };
         
         this.enterTile = function () {
+            console.log("Figur enterTile()");  // <––––––––––––––––––––––––––––––––––
             tileArray[this.onTile].occupiedBy = "";
             this.onTile = this.pathToTravel[0];
             this.pathToTravel.shift();
@@ -38,6 +40,7 @@ class Figure extends Phaser.GameObjects.Sprite {
         };
         
         this.attack = function (enemy) {
+            console.log("Figur attack()");  // <––––––––––––––––––––––––––––––––––
             if (attackButton.mode == "planning cc" && this.stealth == true) { // hinterhältiger Angriff
                 let attackroll = getRandomInt(this.dieSize, this.explodes) + getRandomInt(this.dieSize, this.explodes);
                 if (attackroll >= enemy.def) {
@@ -57,6 +60,7 @@ class Figure extends Phaser.GameObjects.Sprite {
         }
         
         this.moveNow = function () {
+            console.log("moveNow()");  // <––––––––––––––––––––––––––––––––––
             if (this.pathToTravel.length > 0 && this == barb) {
                 movementTweenBarb.data[0].start = this.x;
                 movementTweenBarb.data[1].start = this.y;
@@ -77,7 +81,123 @@ class Figure extends Phaser.GameObjects.Sprite {
         
     }
     
+    activateButtons() {
+        console.log("activateFigure()");  // <––––––––––––––––––––––––––––––––––
+        let buttonXpos = 60;
+        tileArray[this.onTile].checkForNeighbors();
+        
+        faceButton.x = this.x-buttonXpos;
+        faceButton.y = this.y;
+        faceButton.setAlpha(1);
+        
+        // bietet den "laufen-Button" an, wenn ein benachbartes Feld begehbar ist
+        if (tileArray[this.onTile].neighbors.length != 0) {
+            moveButton.x = this.x+buttonXpos;
+            moveButton.y = this.y;
+            moveButton.setAlpha(1);
+            buttonXpos += 85;
+        }
+        
+        // bietet den "Angriffs-Button" an, wenn ein Gegner in Reichweite ist.
+        let adjacentEnemies = false;
+        for (var i = 0; i < tileArray[this.onTile].neighbors.length; i++) {
+            if (tileArray[this.onTile].neighbors[i].occupiedBy == "enemy" || tileArray[this.onTile].neighbors[i].occupiedBy == "idol") {
+                adjacentEnemies = true;
+            }
+        }
+        if (adjacentEnemies == true) {
+            attackButton.x = this.x+buttonXpos;
+            attackButton.y = this.y;
+            attackButton.setAlpha(1);
+            buttonXpos += 85;
+        } else if (this == mage) {
+            let distantEnemies = false;
+            for (var i = 0; i < tileArray.length; i++) {
+                if (lineOfSight(this, i) == true && (tileArray[i].occupiedBy == "enemy" || tileArray[i].occupiedBy == "idol")) {
+                    distantEnemies = true;
+                }
+            }
+            if (distantEnemies == true) {
+                attackButton.x = this.x+buttonXpos;
+                attackButton.y = this.y;
+                attackButton.setAlpha(1);
+                attackButton.mode = "possible rc";
+                buttonXpos += 85;
+            }
+        }
+        
+        // bietet den "Suchen-Button" an.
+        searchButton.x = this.x+buttonXpos;
+        searchButton.y = this.y;
+        searchButton.setAlpha(1);
+        buttonXpos += 85;
+        
+        // bietet den "Tür-Button" an, wenn eine Tür in Reichweite ist.
+        if (tileArray[this.onTile].state == "0dc" || tileArray[this.onTile].state == "0do") {
+            doorButton.x = this.x+buttonXpos;
+            doorButton.y = this.y;
+            doorButton.setAlpha(1);
+            buttonXpos += 85;
+        }
+        
+        // bietet den "special-Button" an, wenn eine Falle auf einem benachbartem Feld ist
+        let adjacentTrap = false;
+        for (var i = 0; i < tileArray[this.onTile].neighbors.length; i++) {
+            if (tileArray[this.onTile].neighbors[i].state == "0t1" && trap1Sprt.alpha !=0 && trap1Sprt.frame != 1) {
+                adjacentTrap = true;
+            }
+        }
+        if ((this == mage || this == rogue) && adjacentTrap == true ) {
+            specialButton.x = this.x+buttonXpos;
+            specialButton.y = this.y;
+            specialButton.setAlpha(1);
+            buttonXpos += 85;
+            specialButton.mode = "disableTrap";
+        }
+        
+        // bietet den "Cancel-Button" an.
+        cancelButton.x = this.x+buttonXpos;
+        cancelButton.y = this.y;
+        cancelButton.setAlpha(1);
+        buttonXpos += 85;
+        
+        switch(this) {
+            case mage:
+                faceButton.setFrame(0);
+                doorButton.setFrame(0);
+                moveButton.setFrame(0);
+                attackButton.setFrame(0);
+                searchButton.setFrame(0);
+                specialButton.setFrame(0);
+                cancelButton.setFrame(0);
+                break;
+            case rogue:
+                faceButton.setFrame(1);
+                doorButton.setFrame(1);
+                moveButton.setFrame(1);
+                attackButton.setFrame(1);
+                searchButton.setFrame(1);
+                specialButton.setFrame(1);
+                cancelButton.setFrame(1);
+                break;
+            case barb:
+                faceButton.setFrame(2);
+                doorButton.setFrame(2);
+                moveButton.setFrame(2);
+                attackButton.setFrame(2);
+                searchButton.setFrame(2);
+                //specialButton.setFrame(2);
+                cancelButton.setFrame(2);
+                break;
+            default:
+                break;
+        }
+        
+        tileArray[this.onTile].neighbors.length = 0;
+    }
+    
     activateFigure() {
+        console.log("activateFigure()");  // <––––––––––––––––––––––––––––––––––
         if (moveButton.mode == "none" && searchButton.mode == "none" && attackButton.mode == "none") {
             deactivateFigures();
 
@@ -85,118 +205,8 @@ class Figure extends Phaser.GameObjects.Sprite {
             this.setFrame(1);
 
             hideActions();
-
-            let buttonXpos = 60;
-            tileArray[this.onTile].checkForNeighbors();
+            activateButtons();
             
-            faceButton.x = this.x-buttonXpos;
-            faceButton.y = this.y;
-            faceButton.setAlpha(1);
-
-            // bietet den "laufen-Button" an, wenn ein benachbartes Feld begehbar ist
-            if (tileArray[this.onTile].neighbors.length != 0) {
-                moveButton.x = this.x+buttonXpos;
-                moveButton.y = this.y;
-                moveButton.setAlpha(1);
-                buttonXpos += 85;
-            }
-
-            // bietet den "Angriffs-Button" an, wenn ein Gegner in Reichweite ist.
-            let adjacentEnemies = false;
-            for (var i = 0; i < tileArray[this.onTile].neighbors.length; i++) {
-                if (tileArray[this.onTile].neighbors[i].occupiedBy == "enemy" || tileArray[this.onTile].neighbors[i].occupiedBy == "idol") {
-                    adjacentEnemies = true;
-                }
-            }
-            if (adjacentEnemies == true) {
-                attackButton.x = this.x+buttonXpos;
-                attackButton.y = this.y;
-                attackButton.setAlpha(1);
-                buttonXpos += 85;
-            } else if (this == mage) {
-                let distantEnemies = false;
-                for (var i = 0; i < tileArray.length; i++) {
-                    if (lineOfSight(this, i) == true && (tileArray[i].occupiedBy == "enemy" || tileArray[i].occupiedBy == "idol")) {
-                        distantEnemies = true;
-                    }
-                }
-                if (distantEnemies == true) {
-                    attackButton.x = this.x+buttonXpos;
-                    attackButton.y = this.y;
-                    attackButton.setAlpha(1);
-                    attackButton.mode = "possible rc";
-                    buttonXpos += 85;
-                }
-            }
-
-            // bietet den "Suchen-Button" an.
-            searchButton.x = this.x+buttonXpos;
-            searchButton.y = this.y;
-            searchButton.setAlpha(1);
-            buttonXpos += 85;
-            
-            // bietet den "Tür-Button" an, wenn eine Tür in Reichweite ist.
-            if (tileArray[this.onTile].state == "0dc" || tileArray[this.onTile].state == "0do") {
-                doorButton.x = this.x+buttonXpos;
-                doorButton.y = this.y;
-                doorButton.setAlpha(1);
-                buttonXpos += 85;
-            }
-            
-            // bietet den "special-Button" an, wenn eine Falle auf einem benachbartem Feld ist
-            let adjacentTrap = false;
-            for (var i = 0; i < tileArray[this.onTile].neighbors.length; i++) {
-                if (tileArray[this.onTile].neighbors[i].state == "0t1" && trap1Sprt.alpha !=0 && trap1Sprt.frame != 1) {
-                    adjacentTrap = true;
-                }
-            }
-            if ((this == mage || this == rogue) && adjacentTrap == true ) {
-                specialButton.x = this.x+buttonXpos;
-                specialButton.y = this.y;
-                specialButton.setAlpha(1);
-                buttonXpos += 85;
-                specialButton.mode = "disableTrap";
-            }
-
-            // bietet den "Cancel-Button" an.
-            cancelButton.x = this.x+buttonXpos;
-            cancelButton.y = this.y;
-            cancelButton.setAlpha(1);
-            buttonXpos += 85;
-
-            switch(this) {
-                case mage:
-                    faceButton.setFrame(0);
-                    doorButton.setFrame(0);
-                    moveButton.setFrame(0);
-                    attackButton.setFrame(0);
-                    searchButton.setFrame(0);
-                    specialButton.setFrame(0);
-                    cancelButton.setFrame(0);
-                    break;
-                case rogue:
-                    faceButton.setFrame(1);
-                    doorButton.setFrame(1);
-                    moveButton.setFrame(1);
-                    attackButton.setFrame(1);
-                    searchButton.setFrame(1);
-                    specialButton.setFrame(1);
-                    cancelButton.setFrame(1);
-                    break;
-                case barb:
-                    faceButton.setFrame(2);
-                    doorButton.setFrame(2);
-                    moveButton.setFrame(2);
-                    attackButton.setFrame(2);
-                    searchButton.setFrame(2);
-                    //specialButton.setFrame(2);
-                    cancelButton.setFrame(2);
-                    break;
-                default:
-                    break;
-            }
-            
-            tileArray[this.onTile].neighbors.length = 0;
         } else if (searchButton.mode == "planning") {
             let activeChar = figuresOnMap[figuresOnMap.findIndex(findActiveChar)];
             if (lineOfSight (activeChar, this.onTile) == true) {
@@ -225,12 +235,14 @@ class Figure extends Phaser.GameObjects.Sprite {
     }
     
     hideFace() {
+        console.log("hideFace()");  // <––––––––––––––––––––––––––––––––––
         if (figuresOnMap.findIndex(findActiveChar) == -1) {
             hideActions();
         }
     }
     
     showFace() {
+        console.log("showFace()");  // <––––––––––––––––––––––––––––––––––
         if (figuresOnMap.findIndex(findActiveChar) == -1) {
             faceButton.x = this.x-60;
             faceButton.y = this.y;
@@ -255,6 +267,7 @@ class Figure extends Phaser.GameObjects.Sprite {
 }
 
 function hideActions() {
+    console.log("hideActions()");  // <––––––––––––––––––––––––––––––––––
     faceButton.setAlpha(0);
     doorButton.setAlpha(0);
     moveButton.setAlpha(0);
@@ -265,6 +278,7 @@ function hideActions() {
 }
 
 function deactivateFigures() {
+    console.log("deactivateFigures()");  // <––––––––––––––––––––––––––––––––––
     for (var i = 0; i < figuresOnMap.length; i++) {
         figuresOnMap[i].active = false;
         figuresOnMap[i].setFrame(0);
