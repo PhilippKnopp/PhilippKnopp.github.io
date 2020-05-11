2ce662fee9295b19cd57365c42c93a562e571e17
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2020-05-11 13:49:10 +0200

    Add files via upload

--- heroines.js
+++ heroines.js
@@ -144,7 +144,7 @@ class Figure extends Phaser.GameObjects.Sprite {
                 this.actions = 0;
                 enemyVisibility();
                 if (barb.health <= 0 && rogue.health <= 0 && mage.health <= 0) {
-                    restartLevel();
+                    //restartLevel();
                 }
             }
         }
