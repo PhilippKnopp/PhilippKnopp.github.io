9f5caf3b7846b069673f53e357556efcb3b129c9
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2020-05-23 21:46:03 +0200

    Add files via upload

--- heroines.js
+++ heroines.js
@@ -41,10 +41,13 @@ class Figure extends Phaser.GameObjects.Sprite {
                 //updateGUI();
                 console.log("XXXXXXXXXXXXXXXXXXXXXXXX Held Move Now");
                 console.log(this.pathToTravel);
+                console.log(movementMarker.x);
                 movementMarker.x = this.x;
                 movementMarker.y = this.y;
+                console.log(movementMarker.x);
                 movementTween.data[0].start = this.x;
                 movementTween.data[1].start = this.y;
+                console.log(movementTween);
                 movementTween.restart();
             } else {
                 console.log("XXXXXXXXXXXXXXXXXXXXXXXX Held Movement Over");
