bfe5fbb927f81c4307ee73c9fc6d2c774e76eac8
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2020-05-23 23:18:06 +0200

    Add files via upload

--- scenes.js
+++ scenes.js
@@ -526,7 +526,9 @@ class SceneGame extends Phaser.Scene {
         // Plane Bewegung
             moveButton.x = this.game.input.activePointer.x + 50;
             moveButton.y = this.game.input.activePointer.y + 50;
-        } else if (moveButton.state == 3) {
+        }
+        
+        if (moveButton.state == 3 && movementTween.isPlaying() == false) {
             activeChar.moveNow();
         }
         
