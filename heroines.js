679e967da0490a8f89043ed1c47fd2213e00757c
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2020-05-31 21:20:48 +0200

    Add files via upload

--- heroines.js
+++ heroines.js
@@ -132,7 +132,7 @@ class Figure extends Phaser.GameObjects.Sprite {
             } else {
                 attackroll += getRandomInt(this.dieSize, this.explodes);
             }
-            rRollText.setTExt(attackroll);
+            rRollText.setText(attackroll);
             
             if (attackroll >= enemy.def) {
                 enemy.health -= attackroll;
