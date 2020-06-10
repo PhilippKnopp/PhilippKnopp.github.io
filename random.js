251f803c3f088747205fa28712ab28a4058108b3
PhilippKnopp <53078162+PhilippKnopp@users.noreply.github.com>
2020-06-11 00:36:38 +0200

    Add files via upload

--- random.js
+++ random.js
@@ -1,13 +1,17 @@
-function getRandomInt(die, explodes) {
-    if (explodes == true) {
-        let roll = Math.floor(Math.random() * die) + 1;
-        let explosioncounter = 1;
-        while (roll%4 == 0) {
-            roll += Math.floor(Math.random() * die) + 1;
-            explosioncounter++;
+function getRandomInt(die, explodes, pool = 1) {
+    let result = 0;
+    for (let i = 0; i < pool; i++) {
+        if (explodes == true) {
+            let roll = Math.floor(Math.random() * die) + 1;
+            let explosioncounter = 1;
+            while (roll%4 == 0) {
+                roll += Math.floor(Math.random() * die) + 1;
+                explosioncounter++;
+            }
+            result += roll;
+        } else {
+            result += Math.floor(Math.random() * die) + 1;
         }
-        return roll;
-    } else {
-        return Math.floor(Math.random() * die) + 1;
     }
+    return result;
 }
